4d56dce9c7419bc4a0ce87b6fbbdf3117c93e492
Tim Ermolt <meatboy0807@gmail.com>
2025-09-04 05:10:56 +0300

    refactor all files

--- GUI.Methods.cs
+++ GUI.Methods.cs
@@ -2,88 +2,93 @@
  * Methods for the primary form GUI class.
 */
 
-namespace SpaceLogs13
+using SpaceLogs13.Classes;
+
+namespace SpaceLogs13;
+
+partial class GUI
 {
-    partial class GUI
+
+    private void AdjustFilePathDisplay()
     {
+        int new_length = FilePathBox.Text.Length * 6;
+        int bb_new_x = new_length;
 
-        private void AdjustFilePathDisplay()
+        FilePathBox.Size = new Size(new_length, FilePathBox.Size.Height);
+        BrowseButton.Location = new Point(bb_new_x, BrowseButton.Location.Y);
+    }
+
+    private void LogsToMainDisplay(string text)
+    {
+        LogDisplay.TextAlign = HorizontalAlignment.Left;
+        LogDisplay.Text = text;
+    }
+
+    private void InfoToMainDisplay(string text)
+    {
+        LogDisplay.TextAlign = HorizontalAlignment.Center;
+        LogDisplay.Text = System.Environment.NewLine + System.Environment.NewLine + System.Environment.NewLine
+                          + System.Environment.NewLine + System.Environment.NewLine + text; // I'm bad at strings ~02.09.2025
+    }
+
+    private void ShowReport(string text)
+    {
+        ReportBox.Visible = true;
+        ReportBox.Text = text;
+    }
+
+    private void ClearReport()
+    {
+        ReportBox.Visible = false;
+        ReportBox.Text = "";
+    }
+
+    private bool AcquireFile(string path)
+    {
+        try
         {
-            int new_length = FilePathBox.Text.Length * 6;
-            int bb_new_x = new_length;
+            current_file = new LogFile(path);
+        }
+        catch (FileNotFoundException e)
+        {
+            MessageBox.Show("Failed to acquire the selected file!" + System.Environment.NewLine + System.Environment.NewLine + e);
+            return false;
+        }
+        string filename = current_file.Filename();
+        FilePathBox.Text = filepath_prefix + filename;
+        AdjustFilePathDisplay();
+        ClearReport();
+        InfoToMainDisplay($"CURRENT FILE: {filename}");
 
-            FilePathBox.Size = new Size(new_length, FilePathBox.Size.Height);
-            BrowseButton.Location = new Point(bb_new_x, BrowseButton.Location.Y);
+        DeselectButton.Enabled = true;
+        SearchButton.Enabled = true;
+        return true;
+    }
+
+    private void UnacquireFile()
+    {
+        if(current_file == null)
+        {
+            return;
         }
 
-        private void LogsToMainDisplay(string text)
+        current_file = null;
+        DeselectButton.Enabled= false;
+        SearchButton.Enabled = false;
+        FilePathBox.Text = filepath_prefix + filepath_default;
+        AdjustFilePathDisplay();
+        ClearReport();
+        InfoToMainDisplay(display_default);
+    }
+
+    private void SearchByKeyword(string keyword, bool match_case = false)
+    {
+        if (current_file == null)
         {
-            LogDisplay.TextAlign = HorizontalAlignment.Left;
-            LogDisplay.Text = text;
+            return;
         }
-
-        private void InfoToMainDisplay(string text)
-        {
-            LogDisplay.TextAlign = HorizontalAlignment.Center;
-            LogDisplay.Text = System.Environment.NewLine + System.Environment.NewLine + System.Environment.NewLine
-                              + System.Environment.NewLine + System.Environment.NewLine + text; // I'm bad at strings ~02.09.2025
-        }
-
-        private void ShowReport(string text)
-        {
-            ReportBox.Visible = true;
-            ReportBox.Text = text;
-        }
-
-        private void ClearReport()
-        {
-            ReportBox.Visible = false;
-            ReportBox.Text = "";
-        }
-
-        private bool AcquireFile(string path)
-        {
-            try
-            {
-                current_file = new LogFile(path);
-            }
-            catch (FileNotFoundException e)
-            {
-                MessageBox.Show("Failed to acquire the selected file!" + System.Environment.NewLine + System.Environment.NewLine + e);
-                return false;
-            }
-            string filename = current_file.Filename();
-            FilePathBox.Text = filepath_prefix + filename;
-            AdjustFilePathDisplay();
-            ClearReport();
-            InfoToMainDisplay($"CURRENT FILE: {filename}");
-
-            DeselectButton.Enabled = true;
-            SearchButton.Enabled = true;
-            return true;
-        }
-
-        private void UnacquireFile()
-        {
-            if(current_file == null)
-            {
-                return;
-            }
-
-            current_file = null;
-            DeselectButton.Enabled= false;
-            SearchButton.Enabled = false;
-            FilePathBox.Text = filepath_prefix + filepath_default;
-            AdjustFilePathDisplay();
-            ClearReport();
-            InfoToMainDisplay(display_default);
-        }
-
-        private void SearchByKeyword(string keyword, bool match_case = false)
-        {
-            string[] found_lines = current_file.Search(keyword, match_case);
-            LogsToMainDisplay(string.Join(System.Environment.NewLine, found_lines));
-            ShowReport($"{current_file.Lines().Length} total lines; {found_lines.Length} matched");
-        }
+        string[] found_lines = current_file.Search(keyword, match_case);
+        LogsToMainDisplay(string.Join(System.Environment.NewLine, found_lines));
+        ShowReport($"{current_file.Lines().Length} total lines; {found_lines.Length} matched");
     }
 }
--- GUI.cs
+++ GUI.cs
@@ -1,75 +1,73 @@
-using System.Text;
-using System.Windows.Forms;
+using SpaceLogs13.Classes;
 
-namespace SpaceLogs13
+namespace SpaceLogs13;
+
+public partial class GUI : Form
 {
-    public partial class GUI : Form
+    private const string filepath_prefix = "Current file: ";
+    private const string filepath_default = "none";
+    private const string display_default = "NO LOG FILE LOADED";
+
+    public LogFile? current_file;
+
+    public GUI()
     {
-        private const string filepath_prefix = "Current file: ";
-        private const string filepath_default = "none";
-        private const string display_default = "NO LOG FILE LOADED";
+        InitializeComponent();
+        this.Select();
 
-        public LogFile? current_file;
-
-        public GUI()
+        FilePathBox.Text = filepath_prefix + filepath_default;
+        AdjustFilePathDisplay();
+    }
+    private void GUI_DragDrop(object sender, DragEventArgs e)
+    {
+        string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
+        if (data == null || !AcquireFile(data[0]))
         {
-            InitializeComponent();
-            this.Select();
-
-            FilePathBox.Text = filepath_prefix + filepath_default;
-            AdjustFilePathDisplay();
+            InfoToMainDisplay("Could not select that file!");
         }
-        private void GUI_DragDrop(object sender, DragEventArgs e)
+    }
+    private void GUI_DragEnter(object sender, DragEventArgs e)
+    {
+        InfoToMainDisplay("Drop the log file to select it");
+        e.Effect = DragDropEffects.Copy; // Otherwise the drag&drop just doesn't work. Thank you Microsoft. ~01.09.2025
+    }
+    private void GUI_DragLeave(object sender, EventArgs e)
+    {
+        InfoToMainDisplay(display_default);
+    }
+
+    private void BrowseButton_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog browser = new();
+        string filter_settings = "Log files|*.txt;*.log|All files|*.*";
+
+        browser.InitialDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName; // this looks like ass ~02.09.2025
+        browser.Title = "Select a log file";
+        browser.Filter = filter_settings;
+        browser.FilterIndex = 1;
+        browser.RestoreDirectory = true;
+
+        if (browser.ShowDialog() == DialogResult.OK)
         {
-            string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
-            if (data == null || !AcquireFile(data[0]))
-            {
-                InfoToMainDisplay("Could not select that file!");
-            }
+            AcquireFile(browser.FileName);
         }
-        private void GUI_DragEnter(object sender, DragEventArgs e)
-        {
-            InfoToMainDisplay("Drop the log file to select it");
-            e.Effect = DragDropEffects.Copy; // Otherwise the drag&drop just doesn't work. Thank you Microsoft. ~01.09.2025
-        }
-        private void GUI_DragLeave(object sender, EventArgs e)
-        {
-            InfoToMainDisplay(display_default);
-        }
+    }
 
-        private void BrowseButton_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog browser = new();
-            string filter_settings = "Log files|*.txt;*.log|All files|*.*";
+    private void SearchButton_Click(object sender, EventArgs e)
+    {
+        SearchByKeyword(KeywordBox.Text, CaseSensitiveCheck.Checked);
+    }
 
-            browser.InitialDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName; // this looks like ass ~02.09.2025
-            browser.Title = "Select a log file";
-            browser.Filter = filter_settings;
-            browser.FilterIndex = 1;
-            browser.RestoreDirectory = true;
+    private void DeselectButton_Click(object sender, EventArgs e)
+    {
+        UnacquireFile();
+    }
 
-            if (browser.ShowDialog() == DialogResult.OK)
-            {
-                AcquireFile(browser.FileName);
-            }
-        }
-
-        private void SearchButton_Click(object sender, EventArgs e)
+    private void Enter(object sender, KeyPressEventArgs e)
+    {
+        if(e.KeyChar == (char)Keys.Enter)
         {
             SearchByKeyword(KeywordBox.Text, CaseSensitiveCheck.Checked);
-        }
-
-        private void DeselectButton_Click(object sender, EventArgs e)
-        {
-            UnacquireFile();
-        }
-
-        private void Enter(object sender, KeyPressEventArgs e)
-        {
-            if(e.KeyChar == (char)Keys.Enter)
-            {
-                SearchByKeyword(KeywordBox.Text, CaseSensitiveCheck.Checked);
-            }
         }
     }
 }
